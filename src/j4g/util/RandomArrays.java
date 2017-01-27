6b7f15ca3c2d48bd4a52372cb019ee339226ca6f
enchf <e.ernesto.espinosa@gmail.com>
2017-01-27 16:42:56 -0600

    Presentation renamed

--- src/j4g/util/RandomArrays.java
+++ src/j4g/util/RandomArrays.java
@@ -1,5 +1,6 @@
 package j4g.util;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -9,5 +10,9 @@ public interface RandomArrays {
 
     static int[] randomArray(int size, int min, int max) {
         return new Random().ints(size, min, max + 1).toArray();
+    }
+
+    public static void main(String...args) {
+        System.out.println(Arrays.toString(randomArray(10,0,100)));
     }
 }
